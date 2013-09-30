23ad931038d6669a501a4268db5c776b91aa3698
Chandan Kumar <chandank.kumar@gmail.com>
2013-09-29 17:05:40 -0700

    added basic header and main.cpp files

---
+++ chatapp.pro
@@ -0,0 +1,11 @@
+LIBS += \
+       -lboost_system\
+       -lpthread\
+       -lboost_thread\
+
+SOURCES += \
+    src/main.cpp
+
+HEADERS += \
+    src/master_header.hpp
+
---
+++ src/master_header.hpp
@@ -0,0 +1,8 @@
+#ifndef MASTER_HEADER_HPP
+#define MASTER_HEADER_HPP
+
+#include <iostream>
+#include <boost/smart_ptr.hpp>
+#include <boost/thread/thread.hpp>
+
+#endif // MASTER_HEADER_HPP
