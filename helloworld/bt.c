02310538433ad5e9daccab48dff37fd0770148af
Chandan kumar <chandank.kumar@gmail.com>
2013-10-09 15:16:41 -0700

    added full prog

--- helloworld/bt.c
+++ helloworld/bt.c
@@ -1,75 +1,136 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
+#include<stdio.h>
 
-struct bt {
-	int m_key;
-	struct bt *left;
-	struct bt *right;
+struct bin_tree {
+int data;
+struct bin_tree * right, * left;
 };
+typedef struct bin_tree node;
 
-typedef struct bt Bt;
-
-void insert(Bt **leaf,int key)
+void insert(node ** tree, int val)
 {
-	Bt *tmp=NULL;
-	if(*leaf ==NULL){
-		printf("Inside NULL\n");
-		tmp=(Bt*)malloc(sizeof(Bt));
-		tmp->left= tmp->right=NULL;
-		tmp->m_key=key;
-		return;
-	}
-	if( key < (*leaf)->m_key){
-		printf("In left \n");
-		insert(&(*leaf)->left,key);
-	}else if( key > (*leaf)->m_key) {
-		printf("In right\n");
-		insert(&(*leaf)->right,key);
-	}
-	
-	return;
+    node *temp = NULL;
+    if(!(*tree))
+    {
+        temp = (node *)malloc(sizeof(node));
+        temp->left = temp->right = NULL;
+        temp->data = val;
+        *tree = temp;
+        return;
+    }
+
+    if(val < (*tree)->data)
+    {
+        insert(&(*tree)->left, val);
+    }
+    else if(val > (*tree)->data)
+    {
+        insert(&(*tree)->right, val);
+    }
+
 }
 
-Bt* search(Bt **leaf, int val)
+void print_preorder(node * tree)
 {
-	if(NULL == *leaf)
-	{
-		return NULL;
-	}
-	if(val == (*leaf)->m_key)
-	{
-		return *leaf;
-	}
-	else if (val < (*leaf)->m_key)
-	{
-		search(&(*leaf)->left,val);
-	} 
-	else if (val < (*leaf)->m_key)
-	{
-		search(&(*leaf)->right,val);
-	}
-	return NULL;
+    if (tree)
+    {
+        printf("%d\n",tree->data);
+        print_preorder(tree->left);
+        print_preorder(tree->right);
+    }
+
 }
 
-void print_pre(Bt **leaf)
+void print_inorder(node * tree)
 {
-	if(*leaf)
-	{
-		printf("%d\n",(*leaf)->m_key);
-		print_pre(&(*leaf)->left);
-		print_pre(&(*leaf)->right);
-	}
+    if (tree)
+    {
+        print_inorder(tree->left);
+        printf("%d\n",tree->data);
+        print_inorder(tree->right);
+    }
 }
 
-int
-main()
+void print_postorder(node * tree)
 {
-	Bt *root=(Bt*)malloc(sizeof(Bt));
-	int i=1;
-	root->m_key=20;
-	root->left=NULL;root->right=NULL;
-	//for(i=0;i<1;i++)
-		insert(&root,i);
-	print_pre(&root);
-	return 1;	
+    if (tree)
+    {
+        print_postorder(tree->left);
+        print_postorder(tree->right);
+        printf("%d\n",tree->data);
+    }
+}
+
+void deltree(node * tree)
+{
+    if (tree)
+    {
+        deltree(tree->left);
+        deltree(tree->right);
+        free(tree);
+    }
+}
+
+node* search(node ** tree, int val)
+{
+    if(!(*tree))
+    {
+        return NULL;
+    }
+
+    if(val < (*tree)->data)
+    {
+        search(&((*tree)->left), val);
+    }
+    else if(val > (*tree)->data)
+    {
+        search(&((*tree)->right), val);
+    }
+    else if(val == (*tree)->data)
+    {
+        return *tree;
+    }
+    return NULL;
+}
+
+int main()
+{
+    node *root;
+    node *tmp;
+    //int i;
+
+    root = NULL;
+    /* Inserting nodes into tree */
+    insert(&root, 9);
+    insert(&root, 4);
+    insert(&root, 15);
+    insert(&root, 6);
+    insert(&root, 12);
+    insert(&root, 17);
+    insert(&root, 2);
+
+    /* Printing nodes of tree */
+    printf("Pre Order Display\n");
+    print_preorder(root);
+
+    printf("In Order Display\n");
+    print_inorder(root);
+
+    printf("Post Order Display\n");
+    print_postorder(root);
+
+    /* Search node into tree */
+    tmp = search(&root, 4);
+    if (tmp)
+    {
+        printf("Searched node=%d\n", tmp->data);
+    }
+    else
+    {
+        printf("Data Not found in tree.\n");
+    }
+
+    /* Deleting all nodes of tree */
+    deltree(root);
+    return 0;
 }
